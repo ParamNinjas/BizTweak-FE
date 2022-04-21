06c433eba796bfbeacaf6dff3996d13a94097efc
kgauzampe <kgauza.mpe@gmail.com>
2022-04-21 13:46:51 +0200

    login page updated

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -4,6 +4,8 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import signin from '../Images/signin.png'
 import Container from '@material-ui/core/Container';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
 import './Login.css'
 
 
@@ -27,17 +29,48 @@ return (
                 </Grid>
                 <Grid item xs={12} sm={12} md={7} lg={7}>
                     <div className="loginForm">
-                        <Typography variant="h3">Login Account</Typography>
-                        <TextField 
-                                id="filled-basic"  
-                                label="Email Address" 
-                                variant="filled" 
-                        />
+                        <div className="formTypo">
+                            <Typography 
+                            variant="h3"
+                            // fontweight='Bold'
+                            >
+                                Login Account
+                            </Typography>  
+                        </div>
+                        <p>
+                        Follow the instructions to make it easier to login and you will be 
+                        able to explore inside.
+                        </p>
+                        <div className="input1">
+                            <TextField 
+                                    id="filled-basic"  
+                                    label="Email Address" 
+                                    variant="filled" 
+                            />
+                        </div>
+                        <div className="input1">
                            <TextField 
                                 id="filled-basic"  
                                 label="Password" 
                                 variant="filled" 
-                        />
+                            />
+                        </div>
+                        <div className="checkP">
+                            <div className="checkBox">
+                                <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+                            </div>
+                            <div className="chackPara">
+                                <p>
+                                    Keep me logged in
+                                </p>
+                            </div>
+                        </div>
+                        <Button 
+                            variant="outlined"
+                            className="Btnlogin"
+                            >
+                                Login
+                        </Button>
                     </div>
                 </Grid>
             </Grid>    

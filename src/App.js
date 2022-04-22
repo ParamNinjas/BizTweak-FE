4fae511e8255b952879df07be61af8eb8fe2e44d
kgauzampe <kgauza.mpe@gmail.com>
2022-04-22 13:18:52 +0200

    Signup page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Login from './Login/Login';
+import Signup from './Signup/Signup'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Signup/>
     </div>
   );
 }

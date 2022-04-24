80757154d80f766251730edd96a1bb58e0bafb21
kgauzampe <kgauza.mpe@gmail.com>
2022-04-24 21:10:59 +0200

    Home page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Login from './Login/Login';
 import Signup from './Signup/Signup'
+import Home from './Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Home/>
     </div>
   );
 }
